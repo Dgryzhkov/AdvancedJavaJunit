f2219d2bc494a02fc515834b218a395538655e88
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-25 15:08:26 +0700

    summaryP2

--- src/main/java/B.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Dgryzhkov
- */
-public class B extends A {
-    @Override
-    public void test() {
-        System.out.println("Hello from class B");
-    }
-}
--- src/main/java/MyClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Dgryzhkov
- */
-public class MyClass {
-    @Deprecated
-    public void myMethod(){
-        System.out.println("Hello from myMethod");
-    }
-}
