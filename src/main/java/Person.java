08413e7780f53433037c4376a3a7ba55066cf2c7
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-25 16:15:24 +0700

    ReflectionP1

---
+++ src/main/java/Person.java
@@ -0,0 +1,40 @@
+/**
+ * @author Dgryzhkov
+ */
+
+@Author(name = "Denis", dateOfCreation = 2022)
+public class Person {
+    private int id;
+    private String name;
+
+    public Person() {
+        this.id = -1;
+        this.name = "No name";
+    }
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void sayMeHello() {
+        System.out.println("Person with id: " + id + " and name: " + name + " say hello");
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Dgryzhkov
- */
-public class Test {
-    @MethodInfo(purpose = "Print Hello World" )
-    public void printHelloWorld(){
-        System.out.println("Hello world");
-    }
-}
