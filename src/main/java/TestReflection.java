8bf1e3e62b3ea27db7abb7bfc20f1a59614803f3
Dgryzhkov <dgryzhkov@gmail.com>
2022-07-27 22:29:42 +0700

    ReflectionP2

--- src/main/java/TestReflection.java
+++ src/main/java/TestReflection.java
@@ -1,36 +1,25 @@
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * @author Dgryzhkov
  */
 public class TestReflection {
-    public static void main(String[] args) throws ClassNotFoundException {
-        Person person = new Person();
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        Scanner scanner = new Scanner(System.in);
+        //название_класса1 название_класса2 название_метода
+        Class classObject1 = Class.forName(scanner.next());
+        Class classObject2 = Class.forName( scanner.next());
+        String methodName = scanner.next();
 
-        Class personClass = Person.class;
-        Class personClass2 = person.getClass();
-        Class personClass3 = Class.forName("Person");
+        Method m =classObject1.getMethod(methodName, classObject2);
+        Object o1= classObject1.newInstance();
+        Object o2 = classObject2.getConstructor(String.class).newInstance("String value");
 
-        Method[] methods = personClass.getMethods();
-/*        for (Method method: methods){
-            System.out.println(method.getName() + ", "
-            + method.getReturnType() +", " + Arrays.toString(method.getParameterTypes()));
-        }*/
+        m.invoke(o1, o2);
 
-/*        Field[] fields = personClass.getDeclaredFields();
-        for (Field field: fields){
-            System.out.println(field.getName() + ", "
-                    + field.getType());
-        }*/
+        System.out.println(o1);
 
-        Annotation[] annotations = personClass.getAnnotations();
-        for (Annotation annotation: annotations){
-            if (annotation instanceof Author) {
-                System.out.println("Yes");
-            };
-        }
     }
 }
